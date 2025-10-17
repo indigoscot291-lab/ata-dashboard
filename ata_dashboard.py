9aeafb4f5c7bb03270fa95c9cd3e06c5305a0795
indigoscot291-lab <indigoscot291@gmail.com>
2025-10-17 10:17:27 -0400

    Update ata_dashboard.py

--- ata_dashboard.py
+++ ata_dashboard.py
@@ -387,7 +387,9 @@ elif page_choice == "National & District Rings":
     import io  # Needed for in-memory fixes
 
     # Correct CSV export URL for Rings sheet
+    #RINGS_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRWVqaFh-t631NUnG02NKhFgIqsoa5xApfWCDp-dwLhJidzk_PSTa8UVrBYCmDlOQ/export?format=csv&gid=410820480"
     RINGS_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRWVqaFh-t631NUnG02NKhFgIqsoa5xApfWCDp-dwLhJidzk_PSTa8UVrBYCmDlOQ/export?format=csv&gid=410820480"
+
     MEMBERS_SHEET_URL = "https://docs.google.com/spreadsheets/d/1aKKUuMbz71NwRZR-lKdVo52X3sE-XgOJjRyhvlshOdM/export?format=csv"
 
     # Load Rings sheet
@@ -473,3 +475,4 @@ elif page_choice == "National & District Rings":
         st.dataframe(results[display_cols].reset_index(drop=True), use_container_width=True, hide_index=True)
     else:
         st.info("No results found. Enter a search term, select a division, or enter a License Number.")
+
