f3f37d47caaad886f563424dc650f48e2ab0297f
indigoscot291-lab <indigoscot291@gmail.com>
2026-06-03 13:33:45 -0400

    Update ata_dashboard.py

--- ata_dashboard.py
+++ ata_dashboard.py
@@ -1531,7 +1531,7 @@ elif page_choice == "National & District Rings":
             else:
                 st.info("No results found. Enter a search term or select an ATA Number.")
        
-        elif event_choice == "Spring Nationals 2026":
+    elif event_choice == "Spring Nationals 2026":
         # Dropdown selector
         section_choice = st.selectbox(
             "Select Category:",
