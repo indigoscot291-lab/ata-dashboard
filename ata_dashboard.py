122c988649f13fb0e7d5859ecb304890ec8b752b
indigoscot291-lab <indigoscot291@gmail.com>
2026-07-10 11:29:50 -0400

    Update ata_dashboard.py

--- ata_dashboard.py
+++ ata_dashboard.py
@@ -1371,7 +1371,7 @@ elif page_choice == "National & District Rings":
                         results = rings_df.loc[mask].copy()
 
             elif search_type == "Division Assigned":
-                div_col = col_map.get("TRADITIONAL RING IDENTIFIER")
+                div_col = col_map.get("Competition Division")
                 if div_col:
                     divisions = sorted(rings_df[div_col].dropna().astype(str).unique())
                     sel_div = st.selectbox("Select Division Assigned (or leave blank):", [""] + divisions)
